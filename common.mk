08b75cb50cdd1be37a0dc5043785aafa00e27255
Pierre Couillaud <pierre@broadcom.com>
2017-10-14 07:50:35 -0700

    avko: update memory layout for new hvd firmware heap.
    refs #SWSTB-5901
    
    Change-Id: Ida9cb17d52ad8bc412a08324564901d6b6684e47
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- common.mk
+++ common.mk
@@ -47,7 +47,7 @@ export BOLT_BOARD_VB             := BCM97252SSFFG_NOAVS
 
 # kernel command line.
 LOCAL_DEVICE_KERNEL_CMDLINE      := mem=984m@0m mem=32m@992m mem=1024m@2048m
-LOCAL_DEVICE_KERNEL_CMDLINE      += bmem=322m@662m bmem=260m@2048m
+LOCAL_DEVICE_KERNEL_CMDLINE      += bmem=324m@660m bmem=260m@2048m
 LOCAL_DEVICE_KERNEL_CMDLINE      += brcm_cma=736m@2308m
 LOCAL_DEVICE_KERNEL_CMDLINE      += ramoops.mem_address=0x3D800000 ramoops.mem_size=0x800000 ramoops.console_size=0x400000
 export LOCAL_DEVICE_KERNEL_CMDLINE
