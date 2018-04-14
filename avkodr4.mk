57a7c0144060c32950fb64866f337bbe929cd825
Pierre Couillaud <pierre@broadcom.com>
2018-04-14 07:51:34 -0700

    avkodr4: equivalent device for 7252sffdr4 hardware support, derivative of avko.
    refs #SWANDROID-4996
    
    Change-Id: If2487d1714331a56c9c17694583e08a28bba500f
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>
    Signed-off-by: Edwin Chan <edwin.chan@broadcom.com>
    (cherry picked from commit 08ecd6825cfedc71ef9931b8115789b1b2e8fc77)

--- avkodr4.mk
+++ avkodr4.mk
@@ -1,17 +1,17 @@
-export LOCAL_PRODUCT_OUT       := avko
+export LOCAL_PRODUCT_OUT       := avkodr4
 
-LOCAL_DEVICE_FSTAB               := device/broadcom/common/fstab/fstab.default.early:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.avko
+LOCAL_DEVICE_FSTAB               := device/broadcom/common/fstab/fstab.default.early:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.avkodr4
 LOCAL_DEVICE_FSTAB               += device/broadcom/common/fstab/fstab.default.early:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.bcm
 export LOCAL_DEVICE_FSTAB
 
 export LOCAL_DEVICE_GPT          := device/broadcom/common/gpts/default.conf
 
-LOCAL_DEVICE_RCS                 := device/broadcom/common/rcs/init.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.avko.rc
+LOCAL_DEVICE_RCS                 := device/broadcom/common/rcs/init.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.avkodr4.rc
 LOCAL_DEVICE_RCS                 += device/broadcom/common/rcs/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
 
-LOCAL_DEVICE_RECOVERY_RCS        := device/broadcom/common/rcs/init.recovery.rc:root/init.recovery.avko.rc
+LOCAL_DEVICE_RECOVERY_RCS        := device/broadcom/common/rcs/init.recovery.rc:root/init.recovery.avkodr4.rc
 
-export LOCAL_DEVICE_AON_GPIO     := device/broadcom/avko/rcs/aon_gpio.cfg:$(TARGET_COPY_OUT_VENDOR)/power/aon_gpio.cfg
+export LOCAL_DEVICE_AON_GPIO     := device/broadcom/avko/rcs/aon_gpio.dr4.cfg:$(TARGET_COPY_OUT_VENDOR)/power/aon_gpio.cfg
 
 # common to all avko devices.
 include device/broadcom/avko/common.mk
@@ -19,14 +19,14 @@ include device/broadcom/avko/common.mk
 # baseline the common support.
 $(call inherit-product, device/broadcom/common/bcm.mk)
 $(call inherit-product, build/make/target/product/product_launched_with_l.mk)
-PRODUCT_NAME                     := avko
-PRODUCT_MODEL                    := avko
-PRODUCT_BRAND                    := google
-PRODUCT_DEVICE                   := avko
+PRODUCT_NAME                     := avkodr4
+PRODUCT_MODEL                    := avkodr4
+PRODUCT_BRAND                    := broadcom
+PRODUCT_DEVICE                   := avkodr4
 
 # additional setup per device.
 PRODUCT_PROPERTY_OVERRIDES += \
-   ro.hardware=avko \
+   ro.hardware=avkodr4 \
    \
    ro.opengles.version=196608 \
    debug.hwui.render_dirty_regions=false \
@@ -50,4 +50,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
    ro.nrdp.modelgroup=AVKO \
    ro.nrdp.validation=ninja_5.1
 
-TARGET_BOOTLOADER_BOARD_NAME := avko
+TARGET_BOOTLOADER_BOARD_NAME := avkodr4
