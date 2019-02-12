672060504548715c21cb011ff04bfa4485cdfc5d
Pierre Couillaud <pierre.couillaud@broadcom.com>
2019-02-11 17:09:32 -0800

    avko: refactor nvi device configuration.
    refs #SWANDROID-6126
    
    Change-Id: Idcc80531d21f301b4c7a247c96a3573fa2f0f884
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>
    (cherry picked from commit b1a39c0a2db608c7c46570a27a6599819bf77387)

--- avko_nvi.mk
+++ avko_nvi.mk
@@ -1,21 +1,21 @@
 # non-vendor-image layout: there is no separate partition for vendor.img in O+.
 export LOCAL_NVI_LAYOUT          := y
 ifndef LOCAL_PRODUCT_OUT
-export LOCAL_PRODUCT_OUT         := avko_nvi
+export LOCAL_PRODUCT_OUT         := avko
 endif
 export TARGET_SAGE_PLATFORM      := avko
 export LOCAL_FULL_TREBLE_NOT_COMPATIBLE := y
 
-LOCAL_DEVICE_FSTAB               := device/broadcom/common/fstab/fstab.nvi.default:root/fstab.avko_nvi
+LOCAL_DEVICE_FSTAB               := device/broadcom/common/fstab/fstab.nvi.default:root/fstab.avko
 LOCAL_DEVICE_FSTAB               += device/broadcom/common/fstab/fstab.nvi.default:root/fstab.bcm
 export LOCAL_DEVICE_FSTAB
 
 export LOCAL_DEVICE_GPT          := device/broadcom/common/gpts/default.nvi.conf
 
-LOCAL_DEVICE_RCS                 := device/broadcom/common/rcs/init.nvi.rc:root/init.avko_nvi.rc
-LOCAL_DEVICE_RCS                 += device/broadcom/common/rcs/ueventd.rc:root/ueventd.avko_nvi.rc
+LOCAL_DEVICE_RCS                 := device/broadcom/common/rcs/init.nvi.rc:root/init.avko.rc
+LOCAL_DEVICE_RCS                 += device/broadcom/common/rcs/ueventd.rc:root/ueventd.avko.rc
 
-LOCAL_DEVICE_RECOVERY_RCS        := device/broadcom/common/rcs/init.recovery.rc:root/init.recovery.avko_nvi.rc
+LOCAL_DEVICE_RECOVERY_RCS        := device/broadcom/common/rcs/init.recovery.rc:root/init.recovery.avko.rc
 
 export LOCAL_DEVICE_AON_GPIO     := device/broadcom/avko/rcs/aon_gpio.cfg:$(TARGET_COPY_OUT_VENDOR)/power/aon_gpio.cfg
 
@@ -28,12 +28,35 @@ include device/broadcom/avko/common.mk
 $(call inherit-product, device/broadcom/common/bcm.mk)
 $(call inherit-product, build/make/target/product/product_launched_with_l.mk)
 PRODUCT_NAME                     := avko_nvi
-PRODUCT_MODEL                    := avko_nvi
-PRODUCT_BRAND                    := broadcom
-PRODUCT_DEVICE                   := avko_nvi
+PRODUCT_MODEL                    := avko
+PRODUCT_BRAND                    := google
+PRODUCT_DEVICE                   := avko
 
 # additional setup per device.
-PRODUCT_PROPERTY_OVERRIDES    += ro.hardware=avko_nvi
-TARGET_BOOTLOADER_BOARD_NAME := avko_nvi
+# additional setup per device.
+PRODUCT_PROPERTY_OVERRIDES += \
+   ro.hardware=avko \
+   \
+   ro.opengles.version=196608 \
+   debug.v3d.use_mma=1 \
+   \
+   ro.nx.heap.video_secure=86m \
+   ro.nx.heap.main=112m \
+   ro.nx.heap.drv_managed=0m \
+   ro.nx.heap.grow=1 \
+   ro.nx.heap.gfx=64m \
+   \
+   ro.nx.capable.cb=1 \
+   ro.nx.capable.si=1 \
+   ro.sf.lcd_density=320 \
+   \
+   ro.rfkilldisabled=1 \
+   ro.nx.eth.irq_mode_mask=3:2 \
+   \
+   ro.com.google.clientidbase=android-avko-tv \
+   \
+   ro.nx.hwc2.tweak.force_eotf=0
+
+TARGET_BOOTLOADER_BOARD_NAME := avko
 # including special configuration for nvi target.
 include device/broadcom/common/nvi.mk
